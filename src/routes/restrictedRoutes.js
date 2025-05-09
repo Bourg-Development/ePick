5a81ac5519f77d1d67b925bd6b91d44d43b9c6ec
bourg <kim@bourg.dev>
2025-05-09 21:02:57 +0200

    Set restricted layout by default in router + added analyses routes

--- src/routes/restrictedRoutes.js
+++ src/routes/restrictedRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authentication');
+const dashboardController = require('../controllers/dashboardController')
 
 router.use(authMiddleware.authenticate);
+router.use((req, res, next) => {
+    res.locals.layout = 'layouts/restricted.ejs';
+    next()
+})
 
 router.get('/dashboard', (req, res) => {
     res.render('dashboard/home', { layout: 'layouts/restricted.ejs', title: 'test', styles: [ '/pages/restricted/dashboard/home.css' ] });
 })
+
+router.get('/dashboard/analyses', dashboardController.analyses)
 
 
 module.exports = router;
